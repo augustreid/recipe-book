900f3488cbad163fdc525eab0de8893f5455d1af
August Reid <august.reid@kin.com>
2022-05-05 11:58:24 -0600

    Add new components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule
